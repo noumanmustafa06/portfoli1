3f824cfc9ce785cb3dc583eab54e904c7ca1ed40
noumanmustafa06 <78797843+noumanmustafa06@users.noreply.github.com>
2021-02-18 17:12:17 +0500

    Add files via upload

---
+++ project # 2/js/main.js
@@ -0,0 +1,150 @@
+"use strict"
+let firstName = "Nouman";
+
+console.log(firstName)
+document.write("my first name is " + firstName + "<br>")
+document.write("this message is a type of  " + typeof firstName)
+document.write("<br>")
+
+let lastName = "mustafa";
+
+console.log(lastName)
+document.write("my last name is "  + lastName + "<br>")
+document.write("this type of message is " + typeof lastName + '<br>')
+document.write("<br>")
+
+let mobileName = "Nokia";
+
+console.log(mobileName)
+document.write("my mobile name is " + mobileName + "<br>")
+document.write("this type of message is " +typeof mobileName )
+document.write("<br>")
+
+let department = "BSCS";
+
+console.log(department)
+document.write("my department name is " + department + "<br>")
+document.write("this type of message is " + typeof department )
+document.write("<br>")
+
+let registrationNumber ="2020-kiu-BS2345";
+
+console.log(registrationNumber)
+document.write("my registration number is " + registrationNumber + "<br>")
+document.write("this message is a type of " +typeof registrationNumber )
+document.write("<br>")
+
+let totalMark = 1100;
+
+console.log(totalMark)
+document.write("total mark is "+ totalMark + "<br>")
+document.write("this message is a type of " + typeof totalMark )
+document.write("<br>")
+
+let gainMark = 900;
+
+console.log(gainMark)
+document.write("marks gained by me is " + gainMark + "<br>")
+document.write("this type of message is " + typeof gainMark )
+document.write("<br>")
+
+let semester = 1;
+
+console.log(semester)
+document.write("i am studying in semester "+ semester + "<br>")
+document.write("this message is a type of " + typeof semester )
+document.write("<br>")
+
+let experience = "Web desinning";
+
+console.log(experience)
+document.write("i have an experience of "+ experience + "<br>")
+document.write("this type of message is " + typeof experience )
+document.write("<br>")
+
+ let grade = "B";
+ 
+ console.log(grade)
+ document.write("i have brought grade is? "  + grade + "<br>")
+ document.write("this message is a type of  " + typeof grade )
+ document.write("<br>")
+
+ let dateOfBirth = "12/11/2000";
+ 
+ console.log(dateOfBirth)
+ document.write("my date of birth is ? " + dateOfBirth + "<br>")
+ document.write("this message is a type of " + typeof dateOfBirth )
+ document.write("<br>")
+
+ let classToper = "true";
+ 
+ console.log(classToper)
+ document.write("i am a class toper. "+ classToper + "<br>")
+ document.write("this message is a type of " + typeof classToper )
+ document.write("<br>")
+
+ let sportInterest = "false";
+ 
+ console.log(sportInterest)
+ document.write("am i intrest in sports game? " + sportInterest + "<br>")
+ document.write("this message is a type of " + typeof sportInterest )
+ document.write("<br>")
+
+ let emailId = "false";
+ 
+ console.log(emailId)
+ document.write("may i have an email id? " + emailId + "<br>")
+ document.write("this message is a type of " + typeof emailId )
+ document.write("<br>")
+
+ let sirName = "joshi";
+ 
+ console.log(sirName)
+ document.write("my sir name is? " + sirName + "<br>")
+ document.write("this message is a type of " + typeof sirName )
+ document.write("<br>")
+
+ let CNIC = "false";
+ 
+ console.log(CNIC)
+ document.write("may i have CNIC? " + CNIC + "<br>")
+ document.write("this message is a type of " + typeof CNIC )
+ document.write("<br>")
+
+ let libraryMember = "ture";
+ 
+ console.log(libraryMember)
+ document.write("may i have the member of library? " + libraryMember + "<br>")
+ document.write("this message is a type of  " + typeof libraryMember )
+ document.write("<br>")
+
+ let fatherName = "wasim khan";
+ 
+ console.log(fatherName)
+ document.write("my father name is? " + fatherName + "<br>")
+ document.write("this message is a type of " + typeof fatherName )
+document.write("<br>")
+ 
+ let motherName = "khatun begum";
+ 
+ console.log(motherName)
+ document.write("my mother name is? " + motherName + "<br>")
+ document.write("this message is a type of " + typeof motherName )
+ document.write("<br>")
+
+ let spotBody = "black dot on back";
+ 
+ console.log(spotBody)
+ document.write("may i have any birth spot on my body? " + spotBody + "<br>")
+ document.write("this message is a type of " + typeof spotBody )
+ document.write("<br>")
+
+ let disease = "polio";
+ 
+ console.log(disease)
+ document.write("may i have any disease by birth? " + disease + "<br>")
+ document.write("this message is a type of " + typeof disease )
+ document.write("<br>")
+ 
+ document.write( "<hr>" + firstName + lastName + mobileName + department + registrationNumber + totalMark + "<br>"  + gainMark + semester + experience + "<br>" + grade + dateOfBirth + classToper + sportInterest + emailId + sirName + "<br>" + CNIC + libraryMember + fatherName + "<br>" + motherName + spotBody + disease + "<hr>"  )
+
